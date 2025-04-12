a65a642cdee833b33820fc15affd5978773b857d
DavidarCZ <94920054+DavidarCZ@users.noreply.github.com>
2025-04-12 17:31:05 +0200

    Set up game
    Added config for Phaser game
    
    Preloaded assets
    
    Added global variables such as game, bird, cursors
    and flag hasLanded
    
    In create function added background, floor, columns,
    bird with gravity and collisions to flag. Initialized
    keyboard cursors.

---
+++ app.js
@@ -0,0 +1,71 @@
+// Setup
+let config = {
+    renderer: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 300 },
+        debug: false
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+};
+// Instance of game
+let game = new Phaser.Game(config);
+
+//load assets
+function preload () {
+    this.load.image('background', 'assets/background.png');
+    this.load.image('road', 'assets/road.png');
+    this.load.image('column', 'assets/column.png');
+    this.load.spritesheet('bird', 'assets/bird.png', { frameWidth: 64, frameHeight: 96 });
+}
+
+//globals
+let bird;
+let hasLanded = false;
+let cursors;
+
+//finalize the preparation phase
+function create () {
+    //bg
+    const background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+
+    //floor
+    const roads = this.physics.add.staticGroup();
+    const road = roads.create(400, 568, 'road').setScale(2).refreshBody();
+    
+
+    //columns
+    const topColumns = this.physics.add.staticGroup({
+        key: 'column',
+        repeat: 1,
+        setXY: { x: 200, y: 0, stepX: 300 }
+    });
+    const bottomColumns = this.physics.add.staticGroup({
+        key: 'column',
+        repeat: 1,
+        setXY: { x: 350, y: 400, stepX: 300 },
+    });
+
+    //bird
+    bird = this.physics.add.sprite(50, 50, 'bird').setScale(2);
+    bird.setBounce(0.2);
+    bird.setCollideWorldBounds(true);
+
+    //collisons
+    this.physics.add.overlap(bird, road, () => hasLanded = true, null, this);
+    this.physics.add.collider(bird, road);
+
+    //controls
+    cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update () {
+}
